355001b9608159fbd5ab925a55fb75c6392d25be
SaidSuyv <sirsuyvi@gmail.com>
2025-10-28 11:05:25 -0500

    update: modify default user seeder account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
+            'lastname' => 'Lastname',
             'email' => 'test@example.com',
         ]);
     }
